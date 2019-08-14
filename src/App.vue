9cdbfe581683a8acbc06fdb4dd8a645b4d4731d8
yy12138 <2980973392@qq.com>
2019-08-14 16:01:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    setTimeout(() => {
+      window.L2Dwidget.init({
+        pluginRootPath: 'static/live2dw/',
+        pluginJsPath: 'lib/',
+        pluginModelPath: 'live2d-widget-model-hijiki/assets/',
+        tagMode: false,
+        debug: false,
+        model: { jsonPath: '/static/live2dw/live2d-widget-model-hijiki/assets/hijiki.model.json' },
+        display: { position: 'right', width: 150, height: 300 },
+        mobile: { show: true },
+        log: false
+      })
+    }, 3000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
